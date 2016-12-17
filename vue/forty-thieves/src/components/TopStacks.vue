0e0d858d5b67a24f39d345916382a84966e55e13
Kenneth Crawford <kmc059000@gmail.com>
2016-12-17 15:39:11 -0600

    top stacks

---
+++ vue/forty-thieves/src/components/TopStacks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="topPanel">
+    <div class="dropZone" id="d0"></div>
+    <div class="dropZone" id="d1"></div>
+    <div class="dropZone" id="d2"></div>
+    <div class="dropZone" id="d3"></div>
+    <div class="dropZone" id="d4"></div>
+    <div class="dropZone" id="d5"></div>
+    <div class="dropZone" id="d6"></div>
+    <div class="dropZone" id="d7"></div>
+    <div class="drawPile"></div>
+    <div class="discardPile"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top-stacks',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
